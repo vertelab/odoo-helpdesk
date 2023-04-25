3b737003bffbc6bf8f6e02de3c65db6aca78034f
Adam Billekvist <adam.billeqvist@outlook.com>
2023-04-25 14:17:26 +0000

    helpdesk_mail_alias: Added loggers to investigate Helpdesk Mail bug

---
+++ helpdesk_mail_alias/__manifest__.py
@@ -0,0 +1,35 @@
+
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Helpdesk Management: Mail Alias",
+    "summary": """Adds email alias support for helpdesk tickets """,
+    "version": "14.0.2.6.0",
+    "license": "AGPL-3",
+    "category": "Helpdesk",
+    "author": "Vertel AB",
+    "website": "https://github.com/vertelab/odoo-helpdesk",
+    "depends": ["helpdesk_mgmt"],
+    "data": [],
+    "demo": [],
+    "application": True,
+    "installable": True,
+}
