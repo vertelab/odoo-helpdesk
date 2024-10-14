eeec952b2b2b7bf4465d28237dad13a2e4f9af94
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2024-10-14 16:15:11 +0200

    helpdesk_duplicate_portal_contact:new transaltion

--- helpdesk_duplicate_portal_contact/models/res_users.py
+++ helpdesk_duplicate_portal_contact/models/res_users.py
@@ -23,14 +23,14 @@ class ResUsers(models.Model):
 
         for user in users:
 
-            num_res_partner = self.env['res.partner'].search_count([("name", '=ilike', user.name)])
+            num_res_partner = self.env['res.partner'].search_count([("email", '=ilike', user.login)])
 
             if num_res_partner >= 1:
 
                 ticket = {
 
                     "name": "Duplicate contacts",
-                    "description": f"Duplicate contacts with the same name. Please combine them.\nUser: {user.name}",
+                    "description": f"Duplicate contacts with the same email. Please combine them.\nUser: {user.login}",
                     "team_id": self.env['ir.config_parameter'].sudo().get_param('helpdesk.helpdesk_team_id'),
                     "company_id": user.company_id.id,
                 }
