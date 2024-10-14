ece8233db4d1a79b8c26713f27de6698e79cdec5
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2024-10-14 17:10:34 +0200

    Code that formats

--- helpdesk_duplicate_portal_contact/models/res_users.py
+++ helpdesk_duplicate_portal_contact/models/res_users.py
@@ -8,31 +8,35 @@ class ResUsers(models.Model):
     _inherit = "res.users"
     _description = "Find duplicate contacts"
 
+
     @api.model_create_multi
     def create(self, vals_list):
-        
-        users = super(ResUsers,self).create(vals_list)
+        users = super(ResUsers, self).create(vals_list)
 
         if self.env['ir.config_parameter'].sudo().get_param('helpdesk.helpdesk_on_duplicate'):
-
-            self.duplicates_create_helpdesk_ticket(users)
+            users.duplicates_create_helpdesk_ticket(users)
 
         return users
 
-    def duplicates_create_helpdesk_ticket(self,users):
+    def duplicates_create_helpdesk_ticket(self, users):
+      for user in users:
+        duplicate_partners = self.env['res.partner'].search([("email", '=ilike', user.login)])
+        if len(duplicate_partners) >= 1:
+            partner_links = "\n".join([f"{partner.name}\n/web#id={partner.id}&model=res.partner&view_type=form\n" for partner in duplicate_partners])
+            _logger.warning(f"{partner_links=}")
+            ticket = {
+                "name": _(f"Duplicate contacts {user.login}"),
+                "description": _("""Duplicate contacts with the same email. Please combine them.
+User: %(user_login)s
 
-        for user in users:
+Duplicate contacts:
+%(partner_links)s""") % {
+                    'user_login': user.login,
+                    'partner_links': partner_links
+                },
+                "team_id": int(self.env['ir.config_parameter'].sudo().get_param('helpdesk.helpdesk_team_id')),
+            }
 
-            num_res_partner = self.env['res.partner'].search_count([("email", '=ilike', user.login)])
+            ticket = self.env["helpdesk.ticket"].create(ticket)
+            _logger.warning(f"{ticket=}")
 
-            if num_res_partner >= 1:
-
-                ticket = {
-
-                    "name": "Duplicate contacts",
-                    "description": f"Duplicate contacts with the same email. Please combine them.\nUser: {user.login}",
-                    "team_id": self.env['ir.config_parameter'].sudo().get_param('helpdesk.helpdesk_team_id'),
-                    "company_id": user.company_id.id,
-                }
-
-                self.env["helpdesk.ticket"].create(ticket)
