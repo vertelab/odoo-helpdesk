df2df26a9be26e21e7b1f32738badd61290f4aef
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-02-11 14:53:54 +0100

    Update __manifest__.py

--- helpdesk_type_sla/__manifest__.py
+++ helpdesk_type_sla/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Helpdesk Type SLA",
+    "name": "Helpdesk: Type SLA",
     "summary": "Helpdesk Type SLA",
     "version": "16.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
